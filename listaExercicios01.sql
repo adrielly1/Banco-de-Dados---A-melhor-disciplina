e3df11aeaeb272729475a0a2de3e09e633bc897d
adrielly1 <143034762+adrielly1@users.noreply.github.com>
2023-09-11 18:09:17 -0300

    Exercicio 11

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -26,3 +26,7 @@ SELECT produto, AVG(receita) as media_receitaFROM vendasGROUP BY produto;
 SELECT produto, SUM(receita) as receita_totalFROM vendasGROUP BY produtoHAVING receita_total > 10000;
 
 SELECT a.nome, COUNT(l.id) as numero_livrosFROM autores aLEFT JOIN livros l ON a.id = l.autor_idGROUP BY a.nomeHAVING numero_livros > 2;
+
+SELECT l.titulo, a.nome
+FROM livros l
+JOIN autores a ON l.autor_id = a.id;
