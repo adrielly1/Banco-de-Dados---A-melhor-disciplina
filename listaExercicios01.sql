289df1359db97d2a01cb0e238a854dd018a666f7
adrielly1 <143034762+adrielly1@users.noreply.github.com>
2023-09-11 18:10:31 -0300

    exercicio 14

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -38,3 +38,5 @@ LEFT JOIN matriculas ON alunos.id = matriculas.aluno_id;
 SELECT a.nome, l.titulo
 FROM autores a
 LEFT JOIN livros l ON a.id = l.autor_id;
+
+SELECT alunos.nome, matriculas.cursoFROM matriculasRIGHT JOIN alunos ON matriculas.aluno_id = alunos.id;
