4a0ac1427124b944340e2b0a34689e5fef655567
adrielly1 <143034762+adrielly1@users.noreply.github.com>
2023-09-26 22:00:28 -0300

    02

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -7,3 +7,23 @@ DELIMITER ;
 CALL sp_ListarAutores();
 
 DELIMITER //
+
+
+CREATE PROCEDURE sp_LivrosPorCategoria(IN categoria_nome VARCHAR(100))
+BEGIN
+    SELECT Livro.Titulo, Autor.Nome, Autor.Sobrenome
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    INNER JOIN Autor_Livro ON Livro.Livro_ID = Autor_Livro.Livro_ID
+    INNER JOIN Autor ON Autor_Livro.Autor_ID = Autor.Autor_ID
+    WHERE Categoria.Nome = categoria_nome;
+END //
+DELIMITER ;
+-- Teste com a categoria "Romance"
+CALL sp_LivrosPorCategoria('Romance');
+-- Teste com a categoria "Ciência"
+CALL sp_LivrosPorCategoria('Ciência');
+-- Teste com a categoria "Ficção Científica"
+CALL sp_LivrosPorCategoria('Ficção Científica');
+
+DELIMITER //
